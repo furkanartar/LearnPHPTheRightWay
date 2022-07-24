c896c5be6e992bd6199b842e47b5b694a9cd8741
Furkan ARTAR <asosyalgenctvtr@gmail.com>
2022-07-24 21:42:25 +0300

    Homework has been completed.

--- public/index.php
+++ public/index.php
@@ -2,10 +2,10 @@
 
 declare(strict_types = 1);
 
-$root = dirname(__DIR__) . DIRECTORY_SEPARATOR;
+$rootDirectory = dirname(__DIR__) . DIRECTORY_SEPARATOR;
 
-define('APP_PATH', $root . 'app' . DIRECTORY_SEPARATOR);
-define('FILES_PATH', $root . 'transaction_files' . DIRECTORY_SEPARATOR);
-define('VIEWS_PATH', $root . 'views' . DIRECTORY_SEPARATOR);
+define('APP_PATH', $rootDirectory . 'app\\' . DIRECTORY_SEPARATOR);
+define('FILES_PATH', $rootDirectory . 'transaction_files\\' . DIRECTORY_SEPARATOR);
+define('VIEWS_PATH', $rootDirectory . 'views\\' . DIRECTORY_SEPARATOR);
 
-/* YOUR CODE (Instructions in README.md) */
+include(VIEWS_PATH . 'transactions.php');
--- views/transactions.php
+++ views/transactions.php
@@ -1,3 +1,11 @@
+<?php
+    $income = 0;
+    $expense = 0;
+    $totalAmount = null;
+
+    include(APP_PATH . 'App.php');
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -34,22 +42,95 @@
                 </tr>
             </thead>
             <tbody>
-                <!-- YOUR CODE -->
+                <?php 
+                    $csvDatas = (getCSVDataFromFile(FILES_PATH, 'csv'));
+                    
+                    $dateColumnNumber = array_search('Date', $csvDatas[0]);
+                    $checkColumnNumber = array_search('Check #', $csvDatas[0]);
+                    $descriptionColumnNumber = array_search('Description', $csvDatas[0]);
+                    $amountColumnNumber = array_search('Amount', $csvDatas[0]);
+
+                    foreach ($csvDatas as $row):
+                        if (is_integer(array_search('Date', $row)))
+                            continue;
+
+                        $amount = (float) stringSeparator($row[$amountColumnNumber], ['$', ',']);
+                ?>
+                   <tr style="text-align: center;">
+                        <td style="width: 25%;"><?= dateFormatter($row[$dateColumnNumber]) ?></td>
+                        <td style="width: 25%;"><?= $row[$checkColumnNumber] ?></td>
+                        <td style="width: 25%;"><?= $row[$descriptionColumnNumber] ?></td>
+                        <td style="width: 25%; <?= ($amount) > 0 ? 'color:green' : 'color:red' ?>">
+                        <?php
+                            if (($amount) > 0) {
+                                $income += $amount;
+                                echo '+$' . number_format($amount, 2, ',', '.');
+                            } else {
+                                $expense += $amount;
+                                $amount = stringSeparator($amount, ['-']);
+                                echo '-$' . number_format($amount, 2, ',', '.');
+                            }
+                        ?></td>
+                    </tr>
+                <?php endforeach ?>
             </tbody>
             <tfoot>
                 <tr>
                     <th colspan="3">Total Income:</th>
-                    <td><!-- YOUR CODE --></td>
+                    <td><?= number_format($income, 2, ',', '.') ?></td>
                 </tr>
                 <tr>
                     <th colspan="3">Total Expense:</th>
-                    <td><!-- YOUR CODE --></td>
+                    <td><?= number_format($expense, 2, ',', '.') ?></td>
                 </tr>
                 <tr>
                     <th colspan="3">Net Total:</th>
-                    <td><!-- YOUR CODE --></td>
+                    <td><?= number_format($income + $expense, 2, ',', '.') ?></td>
                 </tr>
             </tfoot>
         </table>
     </body>
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- 
+
+
+
+
+
+<table style="width:100%">
+    <thead>
+        <th>Date</th>
+        <th>Check</th>
+        <th>Description</th>
+        <th>Amount</th>
+    </thead>
+    <tbody>
+        
+    </tbody>
+</table> -->
